5202994a494ed63da30d5ccf91b90b68c81b5dad
Kenny031782 <ken051782@gmail.com>
2026-02-15 00:09:41 -0800

    Made start button hide the start screen

--- Assets/scripts/Game.cs
+++ Assets/scripts/Game.cs
@@ -2,12 +2,19 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
+    public CanvasGroup StartMenuCanvasGroup;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        CanvasGroupDisplayer.Show(StartMenuCanvasGroup);
     }
-
+    
+    private bool hasGameStarted = false;
+    public void OnStartButtonClick()
+    {
+        CanvasGroupDisplayer.Hide(StartMenuCanvasGroup);
+        hasGameStarted = true;
+    }
     // Update is called once per frame
     void Update()
     {
